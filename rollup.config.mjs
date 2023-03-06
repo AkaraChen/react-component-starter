07cc0683abba280c3d9f7aa8521bd03960f86de9
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-03-06 08:28:42 +0800

    chore: add rollup plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'rollup';
 import typescript from 'rollup-plugin-ts';
 import jsx from 'acorn-jsx';
 import postcss from 'rollup-plugin-postcss';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
     input: 'components/index.ts',
     output: {
         dir: 'dist'
     },
-    plugins: [typescript(), postcss()],
+    plugins: [typescript(), postcss(), nodeResolve(), commonjs()],
     acornInjectPlugins: [jsx()],
-    external: ['react/jsx-runtime']
+    external: id => id.startsWith('react')
 });
